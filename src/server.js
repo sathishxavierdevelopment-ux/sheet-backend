fdf9618ca636d7369e4aaab8f4f97cec5bc7ee8f
sathishxavierdevelopment-ux <sathishxavierdevelopment@gmail.com>
2026-08-08 08:51:10 +0000

    dsdd

--- src/server.js
+++ src/server.js
@@ -20,21 +20,21 @@ connectDB().catch(err => console.error('Database connection error:', err));
 const app = express();
 
 app.use(
-    cors({
-        origin: [
+  cors({
+    origin: [
       "https://sheet-frontend-six.vercel.app",
       "https://placetest.in",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5714"
-        ],
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
-    })
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
 );
 
-app.options(/.*/, cors());
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
